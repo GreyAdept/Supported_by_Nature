6b99b8018a322cc1db0bb3a32fc689ed81274b9f
Adalwolf <64141128+GreyAdept@users.noreply.github.com>
2025-06-30 11:03:36 +0300

    Close game menu added
    + Close game menu that reacts to language

---
+++ Assets/Scripts/UI/ButtonCloseGame.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+
+public class ButtonCloseGame : MonoBehaviour
+{
+    public TextMeshProUGUI buttonText;
+
+    private LanguageManager language;
+
+    public CanvasGroup mainElements;
+
+    public GameObject closeGameMenu;
+
+    private void Start()
+    {
+        language = LanguageManager.Instance;
+        ChangeText();
+    }
+
+    private void ChangeText()
+    {
+        switch (language.currentLanguage)
+        {
+            case (Language.FI):
+                buttonText.text = "Lopeta peli";
+                break;
+            case (Language.SW):
+                buttonText.text = "Avsluta spelet";
+                break;
+            case (Language.EN):
+                buttonText.text = "Quit game";
+                break;
+        }
+    }
+
+    public void ShowCloseGameMenu()
+    {
+        mainElements.alpha = 0;
+        mainElements.interactable = false;
+        closeGameMenu.SetActive(true);
+    }
+}
